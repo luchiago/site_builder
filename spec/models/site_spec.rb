90ec50f7700e26b7d4f6f9a4989d172aa29fa483
Lucas Hiago <lucas.hiago@codeminer42.com>
2020-08-13 20:15:21 -0300

    test(models): add relationship tests for build and site

---
+++ spec/models/site_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Site, type: :model do
+  describe 'relations' do
+    it { is_expected.to have_many(:builds) }
+    it { is_expected.to belong_to(:current_build).class_name('Build') }
+  end
+end
