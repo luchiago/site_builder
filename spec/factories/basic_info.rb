05f11eb4535bd392c413b6246111367c94fe35c8
Lucas Hiago <lucas.hiago@codeminer42.com>
2020-08-16 23:28:21 -0300

    fix: change regex for icon url

--- spec/factories/basic_info.rb
+++ spec/factories/basic_info.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :basic_info, class: Hash do
     defaults = {
       title: Faker::Lorem.sentence,
-      icon: "http://api.thenounproject.com/icon/#{Random.rand(100)}",
+      icon: "https://static.thenounproject.com/svg/#{Random.rand(100)}.svg?Expires=#{Random.rand(999)}",
       background_color: "##{SecureRandom.hex(3)}",
       banner_background_color: "##{SecureRandom.hex(3)}"
     }
