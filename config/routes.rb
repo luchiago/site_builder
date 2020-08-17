54d97271c1e52527e525b686821ea1097cd9d220
Lucas Hiago <lucas.hiago@codeminer42.com>
2020-08-17 13:35:50 -0300

    feat: add urls for sidekiq page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :sites, only: %i[create]
     end
   end
+  mount Sidekiq::Web => '/sidekiq'
 end
