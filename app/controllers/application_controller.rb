b66963e33fb8a8e5c00f2a6a8768b8751000a534
Lucas Hiago <lucas.hiago@codeminer42.com>
2020-08-18 02:28:25 -0300

    feat: add methods to respond

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def render_resource(payload, status = :ok)
+    render_json(payload, status)
+  end
+
+  def not_found_resource(payload = { errors: 'Not Found' }, status = :not_found)
+    render_json(payload, status)
+  end
+
+  def render_errors(payload, status = :bad_request)
+    render_json(payload, status)
+  end
+
+  private
+
+  def render_json(payload = {}, status = :ok)
+    render json: payload, status: status
+  end
 end
