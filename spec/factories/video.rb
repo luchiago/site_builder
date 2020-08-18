ca8fdc38c3a475784a5aa184b1e3f4c41818c007
Lucas Hiago <lucas.hiago@codeminer42.com>
2020-08-18 13:59:35 -0300

    fix: fix warning message for factory constant

--- spec/factories/video.rb
+++ spec/factories/video.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  VIDEO_URLS = ['https://www.youtube.com/embed/Kj_dINCC-cY', 'https://player.vimeo.com/video/77924336'].freeze
-
   factory :video, class: Hash do
     defaults = {
       title: Faker::Lorem.sentence,
-      url: VIDEO_URLS.sample,
+      url: ['https://www.youtube.com/embed/Kj_dINCC-cY',
+            'https://player.vimeo.com/video/77924336'].sample,
       position: Random.rand(12)
     }
 
